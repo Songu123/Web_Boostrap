eaa6f09a82d47e7c53c4c95b44885f72b0702e5c
admin <vans72092@gmail.com>
2024-11-01 10:40:03 +0700

    feat: add function in renderdata.js

--- assets/js/product_detail.js
+++ assets/js/product_detail.js
@@ -10,9 +10,11 @@ function getQueryParam(param) {
 
 // Lấy ID sản phẩm từ URL
 const product_Id = getQueryParam('id');
+renderProducts(product_Id);
+
 
 // Lấy dữ liệu giỏ hàng từ localStorage
-loadProductDetail(product_Id) 
+loadProductDetail(product_Id)
 // Lấy dữ liệu sản phẩm từ localStorage
 
 // Hàm lấy số lượng sản phẩm trong giỏ hàng
@@ -192,7 +194,7 @@ function loadProductDetail(id) {
   container_detail.innerHTML = '';
 
   if (product) {
-    var price = Number(product.price.replace(/\./g, "")); 
+    var price = Number(product.price.replace(/\./g, ""));
     container_detail.innerHTML = `
         <div class="row gx-5">
         <aside class="col-lg-6">
@@ -380,8 +382,87 @@ describe_main.forEach((main, index) => {
   });
 });
 
-
 // End Xử lý ẩn hiện mô tả sản phẩm
+
+// Render sản phẩm liên quan
+
+
+function renderProducts(id) {
+  const products = findProductCategory(id);
+  const productContainer = document.querySelector("#product-related");
+  productContainer.innerHTML = '';
+  products.forEach(product => {
+    productContainer.innerHTML += `
+    <div class="col-md-3 px-3 my-3" style="">
+                    <a href="./product_detail.html?id=${product.id}" class="text-decoration-none">
+                        <div class="card product-card shadow-sm">
+                            <div class="position-relative overflow-hidden">
+                                <div class="img-container w-100">
+                                    <img src="${product.img1}" class="card-img-top"
+                                        alt="Product Image">
+                                    <img src="${product.img2}"
+                                        class="position-absolute card-img-bottom top-0" alt="Product Image">
+                                    <div
+                                        class="position-absolute bottom-0 start-0 text-center w-100 bg-dark text-white p-2 quick-view">
+                                        QUICK VIEW
+                                    </div>
+                                    <button class="loving-product position-absolute top-0 end-0 rounded-circle">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
+                                            fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="">
+                                    <h5 class="card-title">${product.name}</h5>
+                                    <p class="price fw-bold">${product.price} ₫</p>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex">
+                                        <p class="text-muted">Màu sắc</p>
+                                        <div class="d-flex ps-2">
+                                            <img src="./assets/images/products/image.jpg" alt="color1"
+                                                class="rounded-circle me-2" width="30" height="30">
+                                            <img src="./assets/images/products/z5018069269739-aa5c5764d2d4602bb8b55ec50edad312-600x600.jpg"
+                                                alt="color2" class="rounded-circle me-2" width="30" height="30">
+                                            <img src="./assets/images/products/image.jpg" alt="color3"
+                                                class="rounded-circle" width="30" height="30">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>`
+  })
+}
+
+function findProductCategory(productId) {
+  const poloProducts = JSON.parse(localStorage.getItem('polo')) || [];
+  const aothunProducts = JSON.parse(localStorage.getItem('aothun')) || [];
+  const quanthunProducts = JSON.parse(localStorage.getItem('quanthun')) || [];
+  const quanlotProducts = JSON.parse(localStorage.getItem('brief')) || [];
+
+  if (poloProducts.some(product => product.id === productId)) {
+    return poloProducts;
+  } else if (aothunProducts.some(product => product.id === productId)) {
+    return aothunProducts;
+  } else if (quanthunProducts.some(product => product.id === productId)) {
+    return quanthunProducts;
+  } else if (quanlotProducts.some(product => product.id === productId)) {
+  return quanlotProducts;
+} else {
+  return 'Product not found in any category';
+}
+}
+
+// Ví dụ sử dụng:
+const category = findProductCategory(3);
+console.log(category); // Kết quả sẽ là 'T-Shirt Products'
+
 
 
 // Lưu vào giỏ hàng
